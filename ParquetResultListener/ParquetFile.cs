6fc1679f1e6458da3476236aa9cd29cfb9643b15
Frederik Andreasen <frederik.andreasen@keysight.com>
2023-08-18 15:06:50 +0200

    Writing results to parquet

---
+++ ParquetResultListener/ParquetFile.cs
@@ -0,0 +1,39 @@
+﻿using OpenTap;
+using Parquet;
+using Parquet.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParquetResultListener
+{
+    internal sealed class ParquetFile
+    {
+        private readonly string _path;
+        private readonly Schema _schema;
+
+        internal ParquetFile(string path, Schema schema)
+        {
+            _path = path;
+            _schema = schema;
+        }
+
+        internal void PublishResult(ResultTable result)
+        {
+            using Stream stream = File.Open(_path, FileMode.OpenOrCreate);
+            using ParquetWriter writer = new ParquetWriter(_schema, stream, append: false);
+            using ParquetRowGroupWriter groupWriter = writer.CreateRowGroup();
+
+            Dictionary<string, ResultColumn> columns = result.Columns.ToDictionary(c => c.Name, c => c);
+
+            foreach (DataField field in _schema.Fields)
+            {
+                ResultColumn resultColumn = columns[field.Name];
+                DataColumn parquetColumn = new DataColumn(field, resultColumn.Data);
+                groupWriter.WriteColumn(parquetColumn);
+            }
+        }
+    }
+}
---
+++ ParquetResultListener/ParquetResultListener.cs
@@ -0,0 +1,101 @@
+﻿using OpenTap;
+using Parquet.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ParquetResultListener
+{
+    [Display("ParquetResultListener")]
+    public class ParquetResultListener : ResultListener
+    {
+        private Dictionary<Guid, TestPlanRun> _guidToPlanRuns = new Dictionary<Guid, TestPlanRun>();
+        private Dictionary<Guid, TestStepRun> _guidToStepRuns = new Dictionary<Guid, TestStepRun>();
+        private Dictionary<Schema, ParquetFile> _schemaToFiles = new Dictionary<Schema, ParquetFile>();
+
+        public ParquetResultListener()
+        {
+            Name = nameof(ParquetResultListener);
+        }
+
+        public override void Open()
+        {
+            base.Open();
+        }
+
+        public override void Close()
+        {
+            base.Close();
+        }
+
+        public override void OnTestPlanRunStart(TestPlanRun planRun)
+        {
+            base.OnTestPlanRunStart(planRun);
+            _guidToPlanRuns[planRun.Id] = planRun;
+        }
+
+        public override void OnTestPlanRunCompleted(TestPlanRun planRun, Stream logStream)
+        {
+            base.OnTestPlanRunCompleted(planRun, logStream);
+        }
+
+        public override void OnTestStepRunStart(TestStepRun stepRun)
+        {
+            base.OnTestStepRunStart(stepRun);
+            _guidToStepRuns[stepRun.Id] = stepRun;
+        }
+
+        public override void OnTestStepRunCompleted(TestStepRun stepRun)
+        {
+            base.OnTestStepRunCompleted(stepRun);
+        }
+
+        public override void OnResultPublished(Guid stepRunId, ResultTable result)
+        {
+            base.OnResultPublished(stepRunId, result);
+            TestStepRun stepRun = _guidToStepRuns[stepRunId];
+            TestPlanRun planRun = GetPlanRun(stepRun);
+
+            Schema schema = CreateSchemaFromTable(result);
+            if (!_schemaToFiles.TryGetValue(schema, out ParquetFile? file))
+            {
+                string dirName = $"Results/{planRun.TestPlanName}{planRun.StartTime.ToString("yy-MM-dd-HH-mm-ss")}";
+                string fileName = $"{stepRun.TestStepTypeName}.parquet";
+                if (!Directory.Exists(dirName))
+                {
+                    Directory.CreateDirectory(dirName);
+                }
+                file = new ParquetFile($"{dirName}/{fileName}", schema);
+                _schemaToFiles.Add(schema, file);
+            }
+            file.PublishResult(result);
+        }
+
+        private Schema CreateSchemaFromTable(ResultTable result)
+        {
+            List<Field> fields = new List<Field>();
+
+            foreach (ResultColumn column in result.Columns)
+            {
+                if (column.Data is not null && column.Data.Length != 0)
+                {
+                    fields.Add(new DataField(column.Name, column.Data.GetValue(0).GetType()));
+                }
+            }
+
+            Schema schema = new Schema(fields);
+            return schema;
+        }
+
+        private TestPlanRun GetPlanRun(TestStepRun run)
+        {
+            TestPlanRun? planRun;
+            while (!_guidToPlanRuns.TryGetValue(run.Parent, out planRun))
+            {
+                run = _guidToStepRuns[run.Parent];
+            }
+            return planRun;
+        }
+    }
+}
