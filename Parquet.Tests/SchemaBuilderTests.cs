6de17cea06052bba798654b564d854bee019f279
FrederikJA <frederik.andreasen@keysight.com>
2023-11-21 10:44:05 +0100

    Create test project

---
+++ Parquet.Tests/SchemaBuilderTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using OpenTap.Plugins.Parquet;
+
+namespace Parquet.Tests
+{
+    public class SchemaBuilderTests
+    {
+        [Test]
+        public void CreateSchema()
+        {
+        }
+    }
+}
---
+++ Parquet/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parquet.Extensions
+{
+    internal static class DictionaryExtensions
+    {
+        internal static TValue? GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey value)
+        {
+            return dict.TryGetValue(value, out TValue? valueOrNull) ? valueOrNull : default;
+        }
+    }
+}
